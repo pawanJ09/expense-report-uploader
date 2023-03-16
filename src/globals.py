371b9ce4691087981151163863e9ab914995d2bb
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-03-16 10:06:03 -0500

    Adding logic to verify email before proceeding with file upload

--- src/globals.py
+++ src/globals.py
@@ -1,2 +1,3 @@
 bucket_name = 'cc-statements'
 bucket_prefix = 'stmt'
+expense_email_verifier_agw = 'https://fb1v5uqq90.execute-api.us-east-2.amazonaws.com/v1/email-verify'
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from botocore.exceptions import ClientError
-from globals import bucket_name, bucket_prefix
+from globals import bucket_name, bucket_prefix, expense_email_verifier_agw
 import json
 import os
 import base64
 import boto3
+import requests
 
 
 s3_client = boto3.client("s3")
@@ -19,19 +20,33 @@ def lambda_handler(event, context):
         try:
             user_email = event['headers']['user-email']
             print(f'Incoming API Gateway user email: {user_email}')
-            file_content = base64.b64decode(event['body'])
-            file_name = event['headers']['file-name']
-            print(f'Incoming API Gateway file name: {file_name}')
-            s3_response = s3_client.put_object(Bucket=bucket_name,
-                                               Key=bucket_prefix+'/'+user_email+'/'+file_name,
-                                               Body=file_content)
-            print('Returning successful response')
-            msg = {"message": f"{file_name} successfully uploaded."}
-            return {
-                "statusCode": 200,
-                "headers": {"content-type": "application/json"},
-                "body": json.dumps(msg)
-            }
+            print(f'Verifying email from expense-email-verifier service')
+            email_verifier_req = {"user-email": str(user_email)}
+            email_verifier_resp = requests.post(url=expense_email_verifier_agw,
+                                                data=json.dumps(email_verifier_req))
+            if email_verifier_resp.status_code == 200:
+                print('user-email verification successful')
+                file_content = base64.b64decode(event['body'])
+                file_name = event['headers']['file-name']
+                print(f'Incoming API Gateway file name: {file_name}')
+                s3_response = s3_client.put_object(Bucket=bucket_name,
+                                                   Key=bucket_prefix+'/'+user_email+'/'+file_name,
+                                                   Body=file_content)
+                print('Returning successful response')
+                msg = {"message": f"{file_name} successfully uploaded."}
+                return {
+                    "statusCode": 200,
+                    "headers": {"content-type": "application/json"},
+                    "body": json.dumps(msg)
+                }
+            else:
+                print('user-email verification failed')
+                msg = {"message": f"Unable to verify provided user-email."}
+                return {
+                    "statusCode": 404,
+                    "headers": {"content-type": "application/json"},
+                    "body": json.dumps(msg)
+                }
         except KeyError as ke:
             msg = {"message": "Missing user-email, file-name in headers or file content in "
                               "request."}
